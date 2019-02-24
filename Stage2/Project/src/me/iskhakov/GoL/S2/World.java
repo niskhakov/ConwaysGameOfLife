f52e1e36a05ff30de1a58bf3b4f7eb29578bbd96
Nail Iskhakov <nail@iskhakov.me>
2019-02-24 20:56:09 +0300

    World evolution implemented

---
+++ Stage2/Project/src/me/iskhakov/GoL/S2/World.java
@@ -0,0 +1,55 @@
+package me.iskhakov.GoL.S2;
+
+import java.util.Random;
+
+class World {
+    private Cell[][] world;
+
+    public World(int dims) {
+        // If each row contains different amount of cells, consider revising `getNumberOfColumns()` and all calls
+        world = new Cell[dims][dims];
+    }
+
+    public World(int dims, long seed) {
+        this(dims);
+        populateWorld(seed);
+    }
+
+    protected void populateWorld(long seed) {
+        Random random = new Random(seed);
+        for(int i = 0; i < world.length; i++) {
+            for(int j = 0; j < world[i].length; j++) {
+                world[i][j] = new Cell(random.nextBoolean() ? Cell.State.ALIVE : Cell.State.DEAD);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < world.length; i++ ) {
+            for(int j = 0; j < world[i].length; j++) {
+                sb.append(world[i][j].isAlive() ? "O" : " ");
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    public Cell get(int i, int j) {
+        return world[i][j];
+    }
+
+    public void set(int i, int j, Cell value) {
+        world[i][j] = value;
+    }
+
+    public int getNumberOfRows() {
+        return world.length;
+    }
+
+    public int getNumberOfColumns() {
+        return world[0].length;
+    }
+}
